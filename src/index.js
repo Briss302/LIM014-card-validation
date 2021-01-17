20bdc93dbfe201cbd743e62e77d6ef6d53b4a645
Briceyda <Brd0208@gmail.com>
2021-01-17 17:27:55 -0600

    Validación de tarjeta JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import validator from './validator.js';
+import validator from "./validator.js";
+/*function getData() {
+  let card = document.getElementById("inputNumber").value;
+  let name = document.getElementById("inputName").value;
+  alert(card + " " + name);
+};*/
 
-console.log(validator);
+let card = prompt("Ingresa tu numero de tarjeta");
+alert(validator.isValid(card));
+
+/*console.log(validator);*/
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,39 @@
+/*Validator es mi objeto*/
 const validator = {
-  // ...
-};
+  isValid: function (creditCardNumber) {
+    let newString = creditCardNumber
+      .split("")
+      .reverse()
+      .join(""); /*Encadenar los tres metodos juntos*/
+    let validator;
+    let addDigit = 0;
+    let addAll = 0;
 
+    for (let i = 1; i < newString.length; i += 2) {
+      let evenPosition = parseInt(newString.charAt(i) * 2);
+
+      if (evenPosition >= 10) {
+        let stringNumber = evenPosition.toString();
+        addDigit =
+          parseInt(stringNumber.charAt(0)) + parseInt(stringNumber.charAt(1));
+        addAll = addAll + addDigit;
+      } else {
+        addAll = addAll + evenPosition;
+      }
+    }
+
+    for (let i = 0; i < newString.length; i += 2) {
+      let oddposition = parseInt(newString.charAt(i));
+      addAll = addAll + oddposition;
+    }
+
+    if (addAll % 10 == 0) {
+      validator = true;
+    } else {
+      validator = false;
+    }
+    return validator;
+  },
+  maskify: function (creditCardNumber) {},
+};
 export default validator;
