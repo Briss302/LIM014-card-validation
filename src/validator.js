a17d2448384209c1c18d5e8c46a8dd7cc7a8a624
Briceyda <Brd0208@gmail.com>
2021-01-21 18:48:01 -0600

    shopping-And-Card-Validation

--- src/validator.js
+++ src/validator.js
@@ -4,7 +4,9 @@ const validator = {
     let newString = creditCardNumber
       .split("")
       .reverse()
-      .join(""); /*Encadenar los tres metodos juntos*/
+      .join(
+        ""
+      ); /*Encadenar los tres metodos juntos para invertir una cadena de texto*/
     let validator;
     let addDigit = 0;
     let addAll = 0;
@@ -34,6 +36,16 @@ const validator = {
     }
     return validator;
   },
-  maskify: function (creditCardNumber) {},
+  maskify: function (creditCardNumber) {
+    /*el valor creditCardNumber primero se transforma en una matriz de cadena separados, despúes se obtiene todos los numero menos los ultimo cuatro,
+     y en map estos numeros se tranforman en michi obteniendo un nuevo array, por último lo regresamos a que sea un string*/
+    let masked = creditCardNumber
+      .split("")
+      .slice(0, -4)
+      .map(() => "#")
+      .join("");
+    /*Aqui se va concatenar el string convertido en # y el numero ingreado pero solo con sus ultimos cuatro digitos*/
+    return masked + creditCardNumber.slice(-4);
+  },
 };
 export default validator;
